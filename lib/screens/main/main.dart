17d873b77aaaf5fc22362d26dc1d4765f1d10fe5
es <es@MacBook-Pro-Admin-3.local>
2021-11-28 18:24:50 +0300

    refactor: scaffold is added

--- lib/screens/main/main.dart
+++ lib/screens/main/main.dart
@@ -1,8 +1,9 @@
 import 'package:aqohct6w5c4l/blocs/products_bloc/products_bloc.dart';
 import 'package:aqohct6w5c4l/providers/providers.dart';
 import 'package:aqohct6w5c4l/ui/ui.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show AppBar, Icons, Scaffold, Theme;
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'widgets/products_list.dart';
@@ -14,28 +15,41 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarBrightness: theme.colorScheme.brightness,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Меню', style: theme.textTheme.headline1),
+        actions: const [
+          _MainScreenAppBarAddProductAction(),
+        ],
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: theme.colorScheme.brightness,
+        ),
       ),
-      child: _MainSceenBlocsProvider(
-        child: SafeArea(
-          left: false,
-          right: false,
-          bottom: false,
-          child: DecoratedBox(
-            decoration: BoxDecoration(color: theme.colorScheme.background),
-            child: Column(
-              children: const [
-                SizedBox(height: 24),
-                _MainScreenHeader(),
-                SizedBox(height: 8),
-                Expanded(
-                  child: MainScreenProductsList(),
-                ),
-              ],
-            ),
-          ),
+      body: const _MainSceenBlocsProvider(
+        child: MainScreenProductsList(),
+      ),
+      backgroundColor: theme.colorScheme.background,
+    );
+  }
+}
+
+class _MainScreenAppBarAddProductAction extends StatelessWidget {
+  const _MainScreenAppBarAddProductAction({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: PrimaryIconButton(
+          onPressed: () {
+            final bloc = context.read<ProductsBloc>();
+            bloc.add(const ProductsBlocEvent.addOneRandom());
+          },
+          child: Icon(Icons.add, color: theme.colorScheme.onPrimary),
         ),
       ),
     );
@@ -59,32 +73,5 @@ class _MainSceenBlocsProvider extends StatelessWidget {
       },
       child: child,
     );
-  }
-}
-
-class _MainScreenHeader extends StatelessWidget {
-  const _MainScreenHeader({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Меню', style: theme.textTheme.headline1),
-          PrimaryIconButton(
-            onPressed: () {
-              final bloc = context.read<ProductsBloc>();
-              bloc.add(const ProductsBlocEvent.addOneRandom());
-            },
-            child: Icon(Icons.add, color: theme.colorScheme.onPrimary),
-          )
-        ],
-      ),
-    );
-    ;
   }
 }
